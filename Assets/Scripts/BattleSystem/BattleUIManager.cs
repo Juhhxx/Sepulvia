db17621ed3fb8be410d7434259fd717e5d3d7132
Julia Costa <juliacosta232005@gmail.com>
2025-11-13 19:02:56 +0000

    Changes to BattleUIManager

--- Assets/Scripts/BattleSystem/BattleUIManager.cs
+++ Assets/Scripts/BattleSystem/BattleUIManager.cs
@@ -5,7 +5,15 @@ public class BattleUIManager : MonoBehaviour
 {
     [SerializeField] private FillBar _playerFillBar;
     [SerializeField] private FillBar _enemyFillBar;
-    
+
     [SerializeField] private Transform _playerPivot;
     [SerializeField] private Transform _enemyPivot;
+
+    [SerializeField] private GameObject _actionButtons;
+    [SerializeField] private GameObject _moveButtons;
+
+    public void ToogleActionButtons(bool onOff) => _actionButtons.SetActive(onOff);
+    public void ToogleMoveButtons(bool onOff) => _moveButtons.SetActive(onOff);
+
+    
 }
