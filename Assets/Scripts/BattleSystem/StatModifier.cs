f9e7e57e42273385d49627c2143fea0bc9f6b58f
Julia Costa <juliacosta232005@gmail.com>
2025-11-13 01:55:26 +0000

    Add Turn Counting Logic to Stat Modifiers

--- Assets/Scripts/BattleSystem/StatModifier.cs
+++ Assets/Scripts/BattleSystem/StatModifier.cs
@@ -9,4 +9,9 @@ public class StatModifier
     [field: SerializeField] public int AmountAffected { get; private set; }
 
     [field: SerializeField] public int TurnDuration { get; private set; }
+
+    private int _turnsPassed = 0;
+
+    public void TurnPassed() => _turnsPassed++;
+    public bool CheckIfDone() => _turnsPassed == TurnDuration + 1; // Don't count the first turn
 }
