f6e4d3b8359d88ff6114123663fc21043748d68e
Julia Costa <juliacosta232005@gmail.com>
2025-11-13 03:08:52 +0000

    Add ItemInfo sCRIPTABLEoBJECT

--- Assets/Scripts/BattleSystem/ItemInfo.cs
+++ Assets/Scripts/BattleSystem/ItemInfo.cs
@@ -3,5 +3,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "ItemInfo", menuName = "Scriptable Objects/ItemInfo")]
 public class ItemInfo : ScriptableObject
 {
+    [field: SerializeField] public string Name { get; private set; }
     
 }
