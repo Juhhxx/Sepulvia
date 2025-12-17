a38c9161dfa66c350f338ebce69b7ea91cd9084d
Julia Costa <juliacosta232005@gmail.com>
2025-12-17 11:43:47 +0000

    Base Encounter System Done

--- Assets/Scripts/BattleSystem/EncounterSystem/EncounterEntity.cs
+++ Assets/Scripts/BattleSystem/EncounterSystem/EncounterEntity.cs
@@ -1,16 +1,29 @@
+using System;
 using UnityEngine;
 
 public class EncounterEntity : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private PartyInfo _party;
+
+    public event Action<PartyInfo> OnEncounterPlayer;
+
+    private void OnEnable()
     {
-        
+        EncounterManager.Instance.RegsiterEncounterable(this);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        EncounterManager.Instance.UnregsiterEncounterable(this);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerMovement p = other.GetComponent<PlayerMovement>();
+
+        if (p != null)
+        {
+            OnEncounterPlayer?.Invoke(_party);
+        }
     }
 }
--- Assets/Scripts/BattleSystem/EncounterSystem/EncounterManager.cs
+++ Assets/Scripts/BattleSystem/EncounterSystem/EncounterManager.cs
@@ -2,14 +2,27 @@ using NaughtyAttributes;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class EncounterManager : MonoBehaviour
+public class EncounterManager : MonoBehaviourSingleton<EncounterManager>
 {
     [SerializeField, Scene] private string _battleScene;
 
-    private void OnTriggerEnter(Collider other)
+    private void Awake()
     {
-        PlayerMovement p = other.GetComponent<PlayerMovement>();
+        base.SingletonCheck(this);
+    }
 
-        if (p != null) SceneManager.LoadScene(_battleScene);
+    public void RegsiterEncounterable(EncounterEntity entity)
+    {
+        entity.OnEncounterPlayer += DoEncounter;
+    }
+
+    public void UnregsiterEncounterable(EncounterEntity entity)
+    {
+        entity.OnEncounterPlayer -= DoEncounter;
+    }
+
+    private void DoEncounter(PartyInfo party)
+    {
+        Debug.Log($"Doing encounter with {party.PartyName}");
     }
 }
