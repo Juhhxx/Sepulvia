8f6bc886ea45767d5a0f504695963d56e35154f5
Julia Costa <juliacosta232005@gmail.com>
2025-12-30 04:08:12 +0000

    Add Enemy Follow Movment Logic

--- Assets/Scripts/Enemy/EnemyFollowMovement.cs
+++ Assets/Scripts/Enemy/EnemyFollowMovement.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
+using UnityEngine.AI;
 
-public class EnemyFollowMovement : MonoBehaviour
+public class EnemyFollowMovement : MonoBehaviour, IMovementType
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float _followSpeed;
+
+    private NavMeshAgent _agent;
+    private Transform _target;
+
+    public Vector3 Direction => Vector3.zero;
+    public float Speed => _agent.speed;
+
+    private void Start()
     {
-        
+        _target = FindAnyObjectByType<PlayerMovement>().transform;
+
+        _agent = GetComponent<NavMeshAgent>();
+        _agent.updateRotation = false;
+        _agent.speed = _followSpeed;
+        _agent.stoppingDistance = 0.5f;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Move()
     {
-        
+        _agent.destination = _target.position;
     }
 }
