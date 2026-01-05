a385a46ad104d65017f741d7074b52142a8a4c47
Julia Costa <juliacosta232005@gmail.com>
2026-01-05 06:28:53 +0000

    Add Hazard Layer

--- Assets/Scripts/Hazard.cs
+++ Assets/Scripts/Hazard.cs
@@ -1,14 +1,18 @@
+using NaughtyAttributes;
 using UnityEngine;
 
 [RequireComponent(typeof(BoxCollider))]
 public class Hazard : MonoBehaviour
 {
 
-    [SerializeField] private int damageAmount = 5;
+    [SerializeField] private int _damageAmount = 5;
+    [SerializeField, Layer] private int _hazardLayer;
 
     private void Start()
     {
         GetComponent<BoxCollider>().isTrigger = true;
+
+        gameObject.layer = _hazardLayer;
     }
     
     private void OnTriggerEnter(Collider other)
@@ -16,7 +20,7 @@ public class Hazard : MonoBehaviour
         PlayerController player = other.GetComponent<PlayerController>();
         if (player != null)
         {
-            player.PlayerCharacter.CurrentStance -= damageAmount;
+            player.PlayerCharacter.CurrentStance -= _damageAmount;
         }
     }
 }
